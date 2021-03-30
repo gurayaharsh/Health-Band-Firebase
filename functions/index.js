b952a849c56a28dbdfb74677966fbcc45c182b20
gurayaharsh <harsh.guraya98@gmail.com>
2021-03-30 16:37:01 -0400

    enabling FCM communication

--- functions/index.js
+++ functions/index.js
@@ -1,75 +1,58 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const serviceAccount = require("C:/Users/harsh/Downloads/service-account.json"); // must store locally
 
-
-admin.initializeApp();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 const db = admin.firestore();
 
-// App reading Dashboard
-exports.appReadDashboard = functions.https.onRequest((req, res) => {
-  const staffEmail = req.query.email;
 
-  const query = db.collection("staff")
-      .where("email", "==", staffEmail)
-      .limit(1);
-
-  query.get().then((querySnapshot) => {
-    querySnapshot.docs.map((doc) => {
-      const patientList = doc.data().patients;
-      res.status(200).send({
-        "patientList": patientList,
-      });
-    });
-  });
-});
-
-// App reading Info
-exports.appReadInfo = functions.https.onRequest((req, res) => {
-  const patientName = req.query.name;
-
-  const query = db.collection("patients")
-      .where("name", "==", patientName)
-      .limit(1);
-
-  query.get().then((querySnapshot) => {
-    querySnapshot.docs.map((doc) => {
-      const hr = doc.data().hr;
-      const o2 = doc.data().oxygen;
-      const temp = doc.data().temp;
-      const loc = doc.data().location;
-
-      res.status(200).send({
-        "hr": hr,
-        "o2": o2,
-        "temp": temp,
-        "lat": loc.latitude,
-        "lon": loc.longitude,
-      });
-    });
-  });
-});
-
-
-exports.bandWriteInfo = functions.https.onRequest((req, res) => {
+// Band sending alert
+exports.bandSendAlert = functions.https.onRequest((req, res) => {
   const body = req.body;
 
-  const name = body.name;
-  const hr = body.hr;
-  const o2 = body.o2;
-  const temp = body.temp;
-  const loc = new admin.firestore.GeoPoint(body.lat, body.lon);
+  const patient = body.patient;
+  const msg = body.msg;
 
-  const doc = {
-    name: name,
-    hr: hr,
-    oxygen: o2,
-    temp: temp,
-    loc: loc,
-    created: admin.firestore.FieldValue.serverTimestamp(),
-  };
+  sendAlert(patient, msg);
+  
+  res.status(200).send("testing");
 
-  db.collection("patients").add(doc);
-
-  res.status(200).send("sucessfully added doc!");
 });
+
+// FCM communication 
+async function sendAlert(patient, msg) {
+
+ let notifTitle = `Alert: ${patient} is having an emergency!`
+ let notifMsg =  `${msg}`
+
+  const query = db.collection("secrets")
+                  .doc("FCM-Registration-Token"); 
+
+
+ const doc = await query.get(); 
+
+ const registrationToken = doc.data().device;
+ console.log(registrationToken)
+
+  var message = {
+    notification : {
+     title: notifTitle,
+     body : notifMsg
+    }
+  }; 
+
+  admin.messaging().sendToDevice(registrationToken,message)
+  .then((response) => {
+    // Response is a message ID string.
+    console.log('Successfully sent message:', response);
+  })
+  .catch((error) => {
+    console.log('Error sending message:', error);
+  });
+
+}
+
+
